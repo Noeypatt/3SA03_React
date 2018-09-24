e1cc2b5e0f84a2e6d8bc98786717ab67facdf990
Noeypatt <patnarin241240@gmail.com>
2018-09-24 22:41:34 +0700

    lab3_Handler as Props

--- src/WordCard.js
+++ src/WordCard.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import './App.css';
 export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            attempt: '',
+        }
+    }
+    activationHandler = c => {
+        console.log(`${c} has been activated.`)
+    }
     render() {
         return (
-            <div> {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+            <div>
+                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                    activationHandler={this.activationHandler} />)}
             </div>
         );
+    }
+
+    activate = () => {
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
+        }
     }
 } 
