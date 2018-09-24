cae055348b301cfb14dcdc4a0abb4e0fc4b2599d
Noeypatt <patnarin241240@gmail.com>
2018-09-24 23:01:17 +0700

    lab3_Game Logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="AppHandler as Props">
         <WordCard value="hello"/>
       </div>
     );
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 export default class CharacterCard extends Component {
     constructor(props) {
         super(props)
@@ -8,18 +7,23 @@ export default class CharacterCard extends Component {
             active: false,
         }
     }
-    activate = () => {
-
-        if (!this.state.active) {
-            this.setState({ active: true})
+    componentDidUpdate(prevProps) {
+        if (prevProps.attempt != this.props.attempt) {
+            this.setState({ active: false })
         }
     }
-
+    activate = () => {
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
+        }
+    }
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
             <div className={className} onClick={this.activate}>
                 {this.props.value}
-            </div>)
+            </div>
+        )
     }
 }
