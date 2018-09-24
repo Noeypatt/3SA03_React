c7bed42827a54a0d941185bf7ddcb53ad15bfc5a
Noeypatt <patnarin241240@gmail.com>
2018-09-24 22:07:04 +0700

    lab3_Styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 const word = "Hello";
 class App extends Component {
-  render() {
-    return (
-      <div>
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-        }
-      </div>
-    );
-  }
+ render() {
+   return (
+     <div>
+       {
+         Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+       }
+     </div>
+   );
+ }
 }
-
 export default App; 
